6028fc0b2f6ed20fbe98f492ca5386c1861f70a9
AlvaroCaguamon <149434093+AlvaroCaguamon@users.noreply.github.com>
2024-04-08 21:34:02 -0600

    lista para Arbol

---
+++ Prolog/Familiares.pl
@@ -0,0 +1,16 @@
+padre(gilberto, alvaro).
+padre(gilberto, armando).
+padre(gilberto, gilbertojr).
+padre(alvaro, erik).
+padre(alvaro, alvaro). % Te añadí a ti mismo como hijo de Alvaro
+padre(armando, mandin).
+padre(armando, fernando).
+padre(cesar, erik).
+padre(alejandra, erik).
+padre(chucho, erik).
+padre(rosa, carmen).
+padre(rosa, dolores).
+padre(rosa, teresita).
+padre(camilo, carmen).
+padre(camilo, dolores).
+padre(camilo, teresita).
