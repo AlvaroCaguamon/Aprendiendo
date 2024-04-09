60acac5fd181ac1bf0c8013a7a4575d563d2bc96
AlvaroCaguamon <149434093+AlvaroCaguamon@users.noreply.github.com>
2024-04-08 21:31:29 -0600

    Lista de familiares

---
+++ Clisp/arbol.pl
@@ -0,0 +1,16 @@
+padre(gilberto, alvaro).
+padre(gilberto, armando).
+padre(gilberto, gilbertojr).
+padre(alvaro, erik).
+padre(alvaro, alvaro).
+padre(armando, mandin).
+padre(armando, fernando).
+padre(cesar, erik).
+padre(alejandra, erik).
+padre(chucho, erik).
+padre(rosa, carmen).
+padre(rosa, dolores).
+padre(rosa, teresita).
+padre(camilo, carmen).
+padre(camilo, dolores).
+padre(camilo, teresita).
