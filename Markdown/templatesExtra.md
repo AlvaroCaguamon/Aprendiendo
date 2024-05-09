f06b7c1d02c00de2fe6f59028836cda717d32c27
AlvaroCaguamon <149434093+AlvaroCaguamon@users.noreply.github.com>
2024-05-08 20:17:59 -0600

    Terminé templates simples y agregué markdown

---
+++ Markdown/templatesExtra.md
@@ -0,0 +1,62 @@
+%"que onda padrino"
+template([que, onda, padrino], ['Que', 'onda', 'mi', 'amigo', 'que', 'quieres', 'hablar', 'hoy', '?'], []).
+
+%"buenas noches mi rey"
+template([buenas, noches, mi, rey], ['Buenas', 'noches', 'mi', 'rey', 'que', 'descanses', '.'], []).
+
+%"¿Quién es el equipo más grande de México?"
+template([quien, es, el, equipo, mas, grande, de, mexico], ['Obviamente las poderosas e inigualables Águilas del América', '.'], []).
+
+%"Tengo X años de edad"
+template([tengo, s(_), eneros, de, edad], ['Entonces hace ', 0, ' años que no te bañas'], [1]).
+
+%"Nombre"
+template([hola, mi, nombre, es, s(_), '.'], ['Hola', 0, '¿Cómo estás tú?'], [4]).
+
+%"¿Cómo estás?"
+template([como, estas], ['Estoy', 'muy', 'aburrida', ',', '¿y', 'tú', '?'], []).
+
+%"¿Qué haces?"
+template([que, haces], ['Hoy', 'estoy', 'contestando', 'preguntas', 'como', 'siempre', '?'], []).
+
+%"Me duele la cabeza"
+template([me, duele, la, cabeza], ['Un paracetamol cura todo'], []).
+
+%"Estoy aburrido"
+template([estoy, aburrido], ['¿Qué te gustaría hacer para divertirte?'], []).
+
+%"Tengo hambre"
+template([tengo, hambre], ['¿Tienes ganas de comer algo?'], []).
+
+%"Te amo"
+template([te, amo], ['Yo no puedo amar :(', '.'], []).
+
+%"¿Cómo puedo ayudarte?"
+template([como, puedo, ayudarte], ['Estoy aquí para ayudarte a ti'], []).
+
+%"¿Qué opinas de Xochitl?"
+template([que, opinas, de], ['Que si gana se hunde más el país'], []).
+
+%"Tengo miedo"
+template([tengo, miedo], ['A los miedosos no les hacen corridos'], []).
+
+%"Me siento feliz"
+template([me, siento, feliz], ['¡Me alegra mucho oírlo! ¿Qué te hace sentir feliz?'], []).
+
+%"Me siento triste"
+template([me, siento, triste], ['Lamento que te sientas triste. ¿Hay algo que pueda hacer para animarte?'], []).
+
+%"Estoy solo"
+template([estoy, solo], ['¡Animo! Hay muchas personas que se preocupan por ti.'], []).
+
+%"¿Qué piensas del futuro?"
+template([que, piensas, del, futuro], ['El futuro está lleno de posibilidades. Depende de nosotros crearlo de la mejor manera posible'], []).
+
+%"¿Qué es el sentido de la vida?"
+template([que, es, el, sentido, de, la, vida], ['El sentido de la vida es encontrar tu propia felicidad y propósito.'], []).
+
+%"¿Crees en Dios?"
+template([crees, en, dios], ['La fe es una cuestión personal, de tus creencias'], []).
+
+%"¿Cuál es tu comida favorita?"
+template([cual, es, tu, comida, favorita], ['Mi comida favorita son los tacos'], []).
--- Prolog/eliza.pl
+++ Prolog/eliza.pl
@@ -16,12 +16,54 @@ eliza(Input) :-
 	writeln(R),
 	readln(Input1),
 	eliza(Input1), !.
-
+%"que onda padrino"
 template([que, onda, padrino], ['Que', 'onda', 'mi', 'amigo', 'que', 'quieres', 'hablar', 'hoy', '?'], []).
+%"buenas noches mi rey"
 template([buenas, noches, mi, rey], ['Buenas', 'noches', 'mi', 'rey', 'que', 'descanses', '.'], []).
+%quien es el equipo mas grande de mexico
 template([quien, es, el, equipo, mas, grande, de, mexico], ['obviamente las poderosas e inigualables aguilas del america','.'],[]).
+%tengo variable eneros de edad 
 template([tengo, s(_), eneros, de, edad], ['entonces hace ', 0, ' eneros  que no te bañas '], [1]).
+%nombre
 template([hola, mi, nombre, es, s(_), '.'], ['Hola', 0, 'Como', estas, tu, '?'], [4]).
+% "Como estas"
+template([como, estas], ['Estoy', 'muy', 'aburrida', ',', '¿y', 'tú', '?'], []).
+%"Que haces "
+template([que, haces], ['Hoy', 'estoy', 'contestando', 'preguntas', 'como', 
+'siempre' '?'], []).
+% "Me duele la cabeza"
+template([me, duele, la, cabeza], ['un parazetamol cura todo'], []).
+% "Estoy aburrido"
+template([estoy, aburrido], ['¿Qué', 'te', 'gustaría', 'hacer', 'para', 'divertirte', '?'], []).
+%  "Tengo hambre"
+template([tengo, hambre], ['¿Tienes', 'ganas', 'de', 'comer', 'algo', '?'], []).
+%  "Te amo"
+template([te, amo], ['Yo', 'no', 'puedo', 'amar', ':(', '.'], []).
+%  "Como puedo ayudarte"
+template([como, puedo, ayudarte], ['Estoy', 'aquí', 'para', 'ayudarte', 'a', 'ti',], []).
+%  "Que opinas de xochitl"
+template([que, opinas, de], ['que si gana se hunde mas el pais'], []).
+% "Tengo miedo"
+template([tengo, miedo], ['A los miedosos no les hacen corridos '], []).
+%  "Me siento feliz"
+template([me, siento, feliz], ['¡Me', 'alegra', 'mucho', 'oírlo', '!', '¿Qué', 'te', 'hace', 'sentir', 'feliz', '?'], []).
+% "Me siento triste"
+template([me, siento, triste], ['Lamento', 'que', 'te', 'sientas', 'triste', ',', '¿hay', 'algo', 'que', 'pueda',
+'hacer', 'para', 'animarte', '?'], []).
+% Template para "Estoy solo"
+template([estoy, solo], ['por feo estas solo.'], []).
+% "Que piensas del futuro"
+template([que, piensas, del, futuro], ['El', 'futuro', 'está', 'lleno', 'de', 'posibilidades', ',', 'depende',
+ 'de', 'nosotros', 'crearlo', 'de', 'la', 'mejor', 'manera', 'posible', '.'], []).
+% "Que es el sentido de la vida"
+template([que, es, el, sentido, de, la, vida], ['El', 'sentido', 'de', 'la', 'vida', 'es', 
+'Hacer billetes y ser exitoso '], []).
+% "Crees en dios"
+template([crees, en, dios], ['La', 'fe', 'es', 'una', 'cuestión', 'personal',  'de', 'tus', 'creencias', '.'], []).
+%  "Cual es tu comida favorita"
+template([cual, es, tu, comida, favorita], ['Mi', 'comida', 'favorita', 'es','los tacos' ], []).
+
+
 
 
 template([buendia, mi, nombre, es, s(_), '.'], ['buen dia', 'Como', estas, tu, 0, '?'], [4]).
